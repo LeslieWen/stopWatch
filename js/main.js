9ec3b10a8ad9e4c96c9554039a7aaad75f594226
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-13 17:27:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
 
 var timeContainer=document.getElementById("time-display");
-timeContainer.classList.add("hide-me");
+
 
 var btnStart=document.getElementById("btn-start");
 var btnStop=document.getElementById("btn-stop");
 var btnReset=document.getElementById("btn-reset");
 
-var today=new Date();
-var date=today.getFullYear()+"-"+(today.getMonth()+1)+'-'+today.getDate();
-
 
 btnStart.addEventListener("click",function(){
-	renderHTML(Date.now());
+	timeContainer.classList.remove("hide-me");
+	//renderHTML(now);
+	var start=new Date().getTime();	
+	var update = setInterval(function(){
+	 	var now = new Date().getTime();
+		var distance=now-start;
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  		renderHTML(hours,minutes,seconds);
+	},1000); 
 });
 
+//This doesn't work
 btnStop.addEventListener("click",function(){
+	timeContainer.classList.add("hide-me");
 	console.log("stop");	
 });
 
+//This doesn't work
 btnReset.addEventListener("click",function(){
 	resetHTML();
 });
 
-function renderHTML(data){
-	timeContainer.classList.remove("hide-me");
-	var htmlString="<p>"+data+"</p>";
-	timeContainer.innerHTML = data;
-
-	//timeContainer.insertAdjacentHTML('beforeend',htmlString);
+function renderHTML(h,m,s){
+  	timeContainer.innerHTML = h + "h "+ m + "m " + s + "s ";
 }
 
+//This doesn't work
 function resetHTML(){
-	timeContainer.innerHTML="00:00:00"
+	timeContainer.classList.remove("hide-me");
+	timeContainer.innerHTML="0h 0m 0s"
 }
 
 
