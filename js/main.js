b12cfb3b8a26f1be5b7fd1be6c10ace4d9d1ee5d
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-13 16:35:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-var timeContainer=
+
+var timeContainer=document.getElementById("time-display");
+
+var btnStart = document.getElementById("btn-start");
+var btnStop = document.getElementById("btn-stop");
+var btnReset = document.getElementById("btn-reset");
+
+btnStart.addEventListener("click",function(){
+	renderHTML("start")
+});
+
+btnStop.addEventListener("click",function(){
+	console.log("stop");	
+});
+
+btnReset.addEventListener("click",function(){
+	console.log("reset");	
+});
+
+function renderHTML(data){
+	var htmlString="<p>"+data+"</p>";
+	timeContainer.insertAdjacentHTML('beforeend',htmlString);
+}
