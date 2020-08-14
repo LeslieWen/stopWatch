f6f81f3d95a9a3a0a9f60f132554a2a771ab4d63
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-13 20:15:18 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,64 @@
-
 var timeContainer=document.getElementById("time-display");
-
-
 var btnStart=document.getElementById("btn-start");
 var btnStop=document.getElementById("btn-stop");
 var btnReset=document.getElementById("btn-reset");
 
+var flag;
+var firstPress=true;
+
+
+var start;
+var stoppedTime='Null';
 
 btnStart.addEventListener("click",function(){
-	timeContainer.classList.remove("hide-me");
-	//renderHTML(now);
-	var start=new Date().getTime();	
+	
+	//Prevents resetting start time upon pause
+	if(firstPress){
+		start=new Date().getTime();	
+	}
+	firstPress=false;
+	flag=true;
+	
 	var update = setInterval(function(){
-	 	var now = new Date().getTime();
-		var distance=now-start;
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  		renderHTML(hours,minutes,seconds);
+
+	 	if(flag){
+	 		var now = new Date().getTime();
+	 		if(stoppedTime!='Null'){
+	 			now=stoppedTime;
+	 			stoppedTime='Null';
+	 		}
+
+			var distance=now-start;
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  			renderHTML(hours,minutes,seconds);
+  		}
+  		
 	},1000); 
 });
 
 //This doesn't work
 btnStop.addEventListener("click",function(){
-	timeContainer.classList.add("hide-me");
-	console.log("stop");	
+	flag=false; //Flag pauses update setInterval
+	stoppedTime=new Date().getTime();
+	console.log(Math.floor(((stoppedTime-start) % (1000 * 60)) / 1000));
+
 });
 
-//This doesn't work
+
 btnReset.addEventListener("click",function(){
 	resetHTML();
 });
 
-function renderHTML(h,m,s){
+function renderHTML(h,m,s){	
   	timeContainer.innerHTML = h + "h "+ m + "m " + s + "s ";
 }
 
-//This doesn't work
 function resetHTML(){
-	timeContainer.classList.remove("hide-me");
+	flag=false;
 	timeContainer.innerHTML="0h 0m 0s"
+	firstPress=true;
 }
 
 
